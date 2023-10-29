964aaa582fbee898f0a58afc3f9ead1d66a40178
Etumnu Prosper <johnikechukwu94@gmail.com>
2023-10-29 15:44:01 +0100

    feat: Integrated graphql with nestjs server

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import config from './config';
+import config from '../config';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [config], isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
     PrismaModule,
   ],
   controllers: [AppController],
